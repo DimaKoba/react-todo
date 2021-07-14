3a597215ec514434095c5136c053695c8745c878
DimaKoba <azsx07@rambler.ru>
2021-07-14 18:55:09 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import { Home } from './Pages/Home';
 import { Task } from './Pages/Task';
 import { DefaultLayout } from './Components/DefaultLayout';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
     
-      <BrowserRouter>
+      <HashRouter>
         <DefaultLayout>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/task/:id" component={Task} />
           </Switch>
           </DefaultLayout>
-      </BrowserRouter>
+      </HashRouter>
     
       );
 }
